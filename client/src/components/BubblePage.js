99a0bd629f9a1de9934fb658faa783997413228d
Jordan <jordanshehane@gmail.com>
2020-05-02 12:14:16 -0600

    fully functional but it doesnt updcate the page on save of the edit. needs refresh to show updated info. i think its passing though.

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import axiosWithAuth from '../utils/axiosWithAuth';
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
@@ -8,6 +9,20 @@ const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+  useEffect(() =>{
+  axiosWithAuth()
+  .get("/colors")
+  .then(res => {
+    console.log(res.data);
+    setColorList(res.data)
+    return res;
+  })
+  .catch(err => {
+    console.error("error fetching data from api, err: ", err.message);
+    return err;
+  });
+},
+[]);
 
   return (
     <>
--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import axiosWithAuth from '../utils/axiosWithAuth';
 
 const initialColor = {
   color: "",
@@ -11,6 +12,7 @@ const ColorList = ({ colors, updateColors }) => {
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
 
+
   const editColor = color => {
     setEditing(true);
     setColorToEdit(color);
@@ -21,10 +23,26 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    console.log(colorToEdit);
+    axiosWithAuth()
+      .put(`colors/${colorToEdit.id}`, colorToEdit)
+      .then(res =>{
+        console.log(res.data)
+        
+      })
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth()
+      .delete(`colors/${color.id}`)
+      .then(res => {
+        console.log(res);
+        updateColors(colors.filter((color)=> color.id != res.data));
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
 
   return (
